a874fad29492f1c39b931f529ae7fdd9f958d973
Julien Barbay <julien.barbay@gmail.com>
2022-11-15 23:04:45 +0700

    handling of attributes

--- src/dom/createElement.js
+++ src/dom/createElement.js
@@ -1,5 +1,11 @@
-export function createElement(tag, attrs, ...children) {
+export function createElement(tag, attrs = {}, ...children) {
   const el = document.createElement(tag)
+
+  if (attrs) {
+    for (const [name, value] of Object.entries(attrs)) {
+      el.setAttribute(name, value?.toString() ?? name);
+    }
+  }
 
   children.forEach(child => {
     if (typeof child === 'string') {
