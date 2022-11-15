5606dd1eaa84ff86d8cdf98d0de1076acdbb1f04
Julien Barbay <julien.barbay@gmail.com>
2022-11-15 23:07:43 +0700

    strip out unwanted attributes

--- src/dom/createElement.js
+++ src/dom/createElement.js
@@ -1,8 +1,11 @@
+const ignoreAttrs = ['__self', '__source']
+
 export function createElement(tag, attrs = {}, ...children) {
   const el = document.createElement(tag)
 
   if (attrs) {
     for (const [name, value] of Object.entries(attrs)) {
+      if (ignoreAttrs.includes(name)) continue;
       el.setAttribute(name, value?.toString() ?? name);
     }
   }
