8e0749812b3096a30c2f122c5abc29924344b779
Julien Barbay <julien.barbay@gmail.com>
2022-11-15 22:58:30 +0700

    handling textnodes

--- src/dom/createElement.js
+++ src/dom/createElement.js
@@ -2,6 +2,10 @@ export function createElement(tag, attrs, ...children) {
   const el = document.createElement(tag)
 
   children.forEach(child => {
+    if (typeof child === 'string') {
+      child = document.createTextNode(child)
+    }
+
     el.appendChild(child)
   })
 
